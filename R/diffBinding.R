ba8f9c18d71d8f719ad0d67da47dd213ff14cbb1
Georg Stricker <georg.stricker@in.tum.de>
2016-06-29 11:31:32 +0200

    added testing workflow for the correct performance of functions added function to compute region significance

--- R/diffBinding.R
+++ R/diffBinding.R
@@ -4,20 +4,22 @@
 #'
 #' @param fit A GenoGAM object containing the fit
 #' @param regions A GRanges object of regions of interest
+#' @param what Which fit should be used. The names should be equivalent to the column names used in the config file.
+#' Lookup with \code{names(colData(my_GenoGAMDataSet_object))}
 #' @return The GRanges object from the 'region' parameter extended by two columns: pvalue and FDR
 #' @details For a given set of regions, region-wise pvalues are computed by applying
 #' familywise hochberg correction and taking the minimal p-value. FDR is computed by further
 #' applying Benjamini-Hochberg correction.
 #' @author Georg Stricker \email{georg.stricker@@in.tum.de}
 #' @export
-computeRegionSignificance <- function(fit, regions) {
+computeRegionSignificance <- function(fit, regions, what) { ##### <- TODO
     futile.logger::flog.info("Estimating region p-values and FDR")
     
     ov <- data.table::data.table(view(fit, ranges = regions))
     regions_group <- IRanges::findOverlaps(rowRanges(fit), regions)
     ov$gene <- as.factor(subjectHits(regions_group))
     
-    data.table::setnames(ov, "pvalue.s(x):genotype", "pvalue")
+    data.table::setnames(ov, paste("pvalue.s(x)", what, sep = ":"), "pvalue")
     pv = ov[, min(p.adjust(pvalue, method="hochberg")), by = gene]
     
     regions$pvalue = NA
---
+++ exec/run.R
@@ -0,0 +1,4 @@
+functionName <- "computeRegionSignificance"
+source("setup.R")
+source("../R/devel.R")
+source("test_files/test_computeRegionSignificance.R")
