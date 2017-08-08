0ce46c7fde9d226034831907156043167709512b
Georg Stricker <georg.stricker@in.tum.de>
2017-08-08 14:55:01 +0000

    forgot to close bracket in filter.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenoGAM@131846 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filter.R
+++ R/filter.R
@@ -76,7 +76,7 @@ compute_filter <- function(ggd, threshold = NULL, windowsize = 201, mode = c("su
   ## find ranges complying with the threshold
   indx <- which(sums >= threshold)
   if(length(indx) == 0) {
-    return(.extractGR(rowRanges(ggd))
+    return(.extractGR(rowRanges(ggd)))
   }
   poi <- rowRanges(ggd)[indx,]
   diffs <- abs(diff(pos(poi)))
